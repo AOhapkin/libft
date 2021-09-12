268bc2ae8971ee4fdae680a788438e03e954ac56
AOhapkin <alexeyyago@gmail.com>
2021-09-12 16:49:19 +0300

    ft_isalnum

--- libft.h
+++ libft.h
@@ -2,5 +2,7 @@
 # define LIBFT_H
 
 int	ft_isalpha(int c);
+int	ft_isdigit(char c);
+int	ft_isalnum(char c);
 
 #endif
--- srcs/ft_isalnum.c
+++ srcs/ft_isalnum.c
@@ -1,8 +1,12 @@
 #include "../libft.h"
 
-int	ft_isalpha(int c)
+int	ft_isalnum(int c)
 {
 	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}

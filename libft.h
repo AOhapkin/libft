63370fb6032be9133161b41b1c615f2653a4bfd0
AOhapkin <alexeyyago@gmail.com>
2021-09-13 15:38:23 +0300

    ft_memcpy

--- libft.h
+++ libft.h
@@ -11,5 +11,6 @@ int	ft_isprint(int c);
 size_t	ft_strlen(char *str);
 void	*memset(void *dest, int c, size_t count);
 void	ft_bzero(void *b, size_t len);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif

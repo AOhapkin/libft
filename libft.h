1f2e616b1b39fdd7f276efc9897e1f2b4c239a7d
AOhapkin <alexeyyago@gmail.com>
2021-09-14 14:28:44 +0300

    ft_strchr

--- libft.h
+++ libft.h
@@ -17,5 +17,6 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t destsize);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 int	ft_toupper(int c);
 int	ft_tolower(int c);
+char	*ft_strchr(const char *str, int c);
 
 #endif

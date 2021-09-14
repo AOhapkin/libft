d6c76c2e8e7bc2ecdf31ffcade471c0e96d0ed1c
AOhapkin <alexeyyago@gmail.com>
2021-09-14 14:22:12 +0300

    ft_tolower

--- libft.h
+++ libft.h
@@ -16,5 +16,6 @@ void	*ft_memmove(void *dest, void *src, size_t len);
 size_t	ft_strlcpy(char *dest, const char *src, size_t destsize);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 int	ft_toupper(int c);
+int	ft_tolower(int c);
 
 #endif

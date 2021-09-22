5947cab1cbf06e639ec2d7528bfa091e5c90616f
AOhapkin <alexeyyago@gmail.com>
2021-09-22 17:33:03 +0300

    ft_split: strs counter

--- srcs/ft_split.c
+++ srcs/ft_split.c
@@ -1,4 +1,25 @@
+#include "../libft.h"
+
+int	ft_count_strs(char const *s, char c)
+{
+	int	counter;
+	int	i;
+
+	counter = 0;
+	i = 0;
+	while (s[i] == c)
+		i++;
+	while (s[i])
+	{
+		
+	}
+}
+
 char	**ft_split(char const *s, char c)
 {
-	
+	char	**strs;
+	int		i;
+	int		count_strs;
+
+	count_strs = ft_count_strs(s, c);
 }

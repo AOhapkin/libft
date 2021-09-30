7c01442718c7b21c523197259affa6b215f9fc22
AOhapkin <alexeyyago@gmail.com>
2021-09-30 16:31:09 +0300

    ft_split: strs counter fix

--- srcs/ft_split.c
+++ srcs/ft_split.c
@@ -11,8 +11,13 @@ int	ft_count_strs(char const *s, char c)
 		i++;
 	while (s[i])
 	{
-		
+		if ((s[i] != c) && (s[i + 1] == c || s[i + 1] == '\0'))
+		{
+			counter++;
+		}
+		i++;
 	}
+	return (counter);
 }
 
 char	**ft_split(char const *s, char c)

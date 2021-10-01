97357b2c907a7c1ba949008332d363f4c8bc8f7e
AOhapkin <alexeyyago@gmail.com>
2021-10-01 15:10:12 +0300

    ft_split is ready

--- srcs/ft_split.c
+++ srcs/ft_split.c
@@ -20,11 +20,72 @@ int	ft_count_strs(char const *s, char c)
 	return (counter);
 }
 
+int	ft_s_len(char const *s, char c) {
+	int	i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	return (i);
+}
+
+void ft_malloc_error(char **strs)
+{
+	int	i;
+
+	i = 0;
+	while (strs[i])
+	{
+		free(strs[i]);
+		i++;
+	}
+	free(strs);
+	return (NULL);
+}
+
+char	**ft_save_array(char const *s, char c, int strs_number, char **strs)
+{
+	int	i;
+	int	j;
+	int	str_len;
+
+	i = 0;
+	while (i < strs_number)
+	{
+		while (*s == c)
+		{
+			*s++;
+		}
+		str_len = ft_s_len(s, c);
+		strs[i] = (char *)malloc(sizeof(char) * (str_len + 1));
+		if (!strs[i])
+		{
+			ft_malloc_error(strs);
+			return (NULL);
+		}
+		j = 0;
+		while (j < str_len)
+		{
+			strs[i][j] = *s;
+			*s++;
+		}
+		strs[i][j] = '\0';
+		i++;
+	}
+	strs[i] = NULL;
+	return (strs);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**strs;
 	int		i;
-	int		count_strs;
+	int		strs_number;
 
-	count_strs = ft_count_strs(s, c);
+	strs_number = ft_count_strs(s, c);
+	strs = (char **)malloc(sizeof(char *) * (strs_number + 1));
+	if (!strs)
+		return (NULL);
+	strs = ft_save_array(s, c, strs_number, strs);
+	return (strs);
 }

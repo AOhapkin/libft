6ea36bd81637ff21029f533cf0c9b67d908e6241
AOhapkin <alexeyyago@gmail.com>
2021-10-02 14:36:25 +0300

    ft_itoa

--- srcs/ft_itoa.c
+++ srcs/ft_itoa.c
@@ -15,9 +15,29 @@ int	ft_n_len(int n)
 
 char	*ft_itoa(int n)
 {
+	char	*dest;
+	int	i;
+	int	n_len;
+
+	i = 0;
+	n_len = ft_n_len(n);
 	if (n == -2147483648)
 	{
-		return (ft_strdup("-2147483648"));
+		return (ft_strdup('-2147483648'));
 	}
-	
+	if (ft_isnegative(n))
+	{
+		dest[i] = '-';
+		n *= -n;
+	}
+	i = n_len;
+	while (n > 9)
+	{
+		dest[i] = n % 10 + '0';
+		n /= 10;
+		i--;
+	}
+	dest[i] = n % 10 + '0';
+	dest[n_len] = '\0';
+	return (dest);
 }

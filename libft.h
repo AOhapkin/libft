5f408a50b51bcdf2ad30cfc59b31746e0aca7b37
AOhapkin <alexeyyago@gmail.com>
2021-10-03 15:55:49 +0300

    ft_strmapi

--- libft.h
+++ libft.h
@@ -36,5 +36,6 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 int	ft_isnegative(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 #endif

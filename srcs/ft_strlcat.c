04829ad414a2841706ec5035875c9178c497a4c4
gmyriah <gmyriah@student.21-school.ru>
2021-10-07 14:53:35 +0300

    ft_strlcat fix

--- srcs/ft_strlcat.c
+++ srcs/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: gmyriah <gmyriah@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 15:27:07 by gmyriah           #+#    #+#             */
-/*   Updated: 2021/10/05 15:27:28 by gmyriah          ###   ########.fr       */
+/*   Updated: 2021/10/07 14:51:56 by gmyriah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,27 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
-	size_t	dest_size;
-	size_t	src_size;
+	size_t	dest_len;
+	size_t	src_len;
 	size_t	i;
 	size_t	j;
 
-	dest_size = ft_strlen(dest);
-	src_size = ft_strlen(src);
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (size == 0 || size <= dest_len)
+		return (size + src_len);
 	i = 0;
 	j = 0;
 	while (dest[i])
-		i++;
-	while (src[j] && j < (size - (dest_size - 1)))
 	{
-		dest[j] = src[j];
+		i++;
+	}
+	while (src[j] != '\0' && j < (size - dest_len - 1))
+	{
+		dest[i] = src[j];
+		i++;
 		j++;
 	}
-	dest[j] = '\0';
-	return (dest_size + src_size);
+	dest[i] = '\0';
+	return (dest_len + src_len);
 }

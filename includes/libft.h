fcfc803cae1a6f33f260b42c00811704af9e88db
AOhapkin <alexeyyago@gmail.com>
2021-10-08 15:15:44 +0300

    ft_putchar_fd

--- includes/libft.h
+++ includes/libft.h
@@ -48,5 +48,6 @@ size_t	ft_long_len(long n);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
 
 #endif

fc640cc0213fdd7d5e1d037e5e33073c6cb8e0c9
gmyriah <gmyriah@student.21-school.ru>
2021-10-09 12:51:57 +0300

    ft_strnstr del comments

--- srcs/ft_strnstr.c
+++ srcs/ft_strnstr.c
@@ -14,29 +14,10 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-//	size_t	j;
-//	char	*h;
-//	char	*n;
 	size_t	needle_len;
 
-//	h = (char *)(haystack);
 	if (*needle == '\0')
 		return ((char *)haystack);
-//	while (*haystack != '\0' && len != 0)
-//	{
-//		j = len;
-//		while (*haystack == *needle && *needle != '\0' && j != 0)
-//		{
-//			needle++;
-//			haystack++;
-//			j--;
-//
-//		}
-//		if (*needle == '\0')
-//			return ((char *)haystack);
-//		haystack++;
-//		len--;
-//	}
 	needle_len = ft_strlen(needle);
 	while (len >= needle_len)
 	{

d372b18578a825a9db35a399809d734a0953efc3
gmyriah <gmyriah@student.21-school.ru>
2021-10-09 13:23:13 +0300

    ft_substr protected

--- srcs/ft_substr.c
+++ srcs/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: gmyriah <gmyriah@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 17:08:23 by gmyriah           #+#    #+#             */
-/*   Updated: 2021/10/09 11:52:04 by gmyriah          ###   ########.fr       */
+/*   Updated: 2021/10/09 13:19:38 by gmyriah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,24 @@ char	*ft_substr(char const *str, unsigned int start, size_t len)
 {
 	char	*substr;
 	size_t	i;
+	size_t	j;
 
-	substr = malloc(sizeof(char) * len);
+	if (!str)
+		return (NULL);
+	substr = malloc(sizeof(*str) * (len + 1));
 	if (!substr)
 		return (NULL);
 	i = 0;
-	while (len)
+	j = 0;
+	while (str[i])
 	{
-		substr[i] = str[i + start];
-		len--;
+		if (i >= start && j < len)
+		{
+			substr[j] = str[i];
+			j++;
+		}
 		i++;
 	}
+	substr[len] = '\0';
 	return (substr);
 }

36d29ef97b2c3798a107c9fe1a3f452cdc5dc443
AOhapkin <alexeyyago@gmail.com>
2021-10-11 14:53:34 +0300

    ft_memmove fix

--- ft_memmove.c
+++ ft_memmove.c
@@ -16,11 +16,9 @@ void	*ft_memmove(void *dest, const void *src, size_t len)
 {
 	unsigned char		*d;
 	const unsigned char	*s;
-	size_t				i;
 
 	d = (unsigned char *)dest;
 	s = (unsigned char *)src;
-	i = 0;
 	if (len == 0 || s == d)
 		return (dest);
 	if (d > s)

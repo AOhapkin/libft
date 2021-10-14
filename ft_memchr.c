8bc2a33be068dd7e32666743f58068758345e2d9
Glass Myriah <gmyriah@ga-d1.msk.21-school.ru>
2021-10-14 18:19:07 +0300

    ft_memchr fix

--- ft_memchr.c
+++ ft_memchr.c
@@ -14,13 +14,13 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t			i;
+	size_t	i;
 
 	i = 0;
 	while (i < n)
 	{
-		if (*((char *)s + i) == c)
-			return ((char *)s + i);
+		if (*((unsigned char *)s + i) == (unsigned char)c)
+			return ((unsigned char *)s + i);
 		i++;
 	}
 	return (NULL);

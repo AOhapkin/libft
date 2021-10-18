4a7b10136bc9a4637fc17f02ce1806636b463d74
AOhapkin <alexeyyago@gmail.com>
2021-10-18 10:34:30 +0300

    fix ft_split with ft_substr

--- ft_split.c
+++ ft_split.c
@@ -43,50 +43,33 @@ int	ft_count_strs(char const *s, char c)
 	return (n);
 }
 
-char	*ft_push_str(char *s, char c)
-{
-	char	*str;
-	int		i;
-
-	i = 0;
-	while (s[i] && s[i] != c)
-		i++;
-	str = (char *)malloc(sizeof(char) * (i));
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (*s && *s != c)
-	{
-		str[i] = *s;
-		i++;
-		s++;
-	}
-	return (str);
-}
-
 char	**ft_split(char const *s, char c)
 {
 	char	**result;
-	int		i;
+	char const	*substr_ptr;
+	size_t	i;
+	size_t	substr_len;
 
-	i = 0;
 	if (!s)
 		return (NULL);
 	result = (char **) malloc(sizeof(char *) * (ft_count_strs(s, c) + 1));
 	if (!result)
 		return (NULL);
+	i = 0;
 	while (*s)
 	{
 		while (*s == c)
 			s++;
 		if (*s)
 		{
-			result[i] = ft_push_str((char *)s, c);
-			if (!result[i])
-				ft_split_error(result);
-			i++;
-			while (*s && *s != c)
-				s++;
+			substr_ptr = s;
+			substr_len = 0;
+			while (*s && *s++ != c)
+				substr_len++;
+			s--;
+			if (*s != c)
+				if (!(result[i++] = ft_substr(substr_ptr, 0, substr_len)))
+					ft_split_error(result);
 		}
 	}
 	result[i] = 0;

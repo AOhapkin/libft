cdc7c6292c0317d9f11dba4ea197a7b06a2c08a6
AOhapkin <alexeyyago@gmail.com>
2021-10-19 13:16:49 +0300

    ft_memmove norm fix

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: gmyriah <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 12:37:11 by gmyriah           #+#    #+#             */
-/*   Updated: 2021/10/19 12:37:11 by gmyriah          ###   ########.fr       */
+/*   Updated: 2021/10/19 13:15:47 by gmyriah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ void	*ft_memmove(void *dest, const void *src, size_t len)
 
 	d = (unsigned char *)dest;
 	s = (unsigned char *)src;
-	if (len == 0)
-		return (dest);
 	if (d > s)
 	{
 		d += len - 1;
